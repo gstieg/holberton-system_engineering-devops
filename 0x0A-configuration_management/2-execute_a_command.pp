a19b8a59787179b302db4988cc75ee3a334d0a1b
Grayson Stiegler <682@holbertonschool.com>
2019-08-06 00:27:58 +0000

    fix path

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,5 @@
 # create a manifest that kills a process named killmenow
-exec { 'killmenow':
-  command => 'pkill killmenow',
+exec { 'killmenow' :
+  path => '/usr/bin',
+  command => 'pkill killmenow'
 }
