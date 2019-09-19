c6953e9adfb6ea05213f1290bb85f298f92df5d6
Grayson Stiegler <682@holbertonschool.com>
2019-09-19 22:25:53 +0000

    first commmit for subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Reddit API and returns the number of subscribers"""
+
+import requests
+
+def number_of_subscribers(subreddit):
+    usr = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) \
+    AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.75 Safari/537.36'}
+    url = requests.get('https://www.reddit.com/r/{}/about.json'.format\
+                       (subreddit))
+
+    try:
+        return url.json()['data']['subscribers']
+    except Exception:
+        return 0
