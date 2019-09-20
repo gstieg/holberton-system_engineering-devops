fe95bd78916e4d1f84f6ba9fb1215738105b1bfd
Grayson Stiegler <682@holbertonschool.com>
2019-09-20 04:53:22 +0000

    fix pep8

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,10 +3,11 @@
 
 import requests
 
+
 def number_of_subscribers(subreddit):
     usr = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) \
     AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.75 Safari/537.36'}
-    url = requests.get('https://www.reddit.com/r/{}/about.json'.format\
+    url = requests.get('https://www.reddit.com/r/{}/about.json'.format
                        (subreddit))
 
     try:
