c1957929b456811c2a4bd4be69c178f42dbc0fc0
Grayson Stiegler <682@holbertonschool.com>
2019-09-20 05:12:14 +0000

    add to save

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""prints the titles of the first 10 hot posts listed for a given subreddit."""
+
+import requests
+
+
+def top_ten(subreddit):
+     usr = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) \
+AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.75 Safari/537.36'}
+url = requests.get('https://www.reddit.com/r/{}/about.json'.format
+                   (subreddit), allow_redirects=False)
+u = url.json()
+for i in range(10):
+      try:
+      print(u.get('data').get('children')[i].get('data').get('title'))
+except Exception:
+      print(None)
