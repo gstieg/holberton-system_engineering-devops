10574fa5c5a64a76c575cbd18639f05756ef9d53
Grayson Stiegler <682@holbertonschool.com>
2019-09-20 06:29:36 +0000

    fix syntax

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,6 +5,8 @@ import requests
 
 
 def number_of_subscribers(subreddit):
+    """count of how many subscribers"""
+
     usr = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) \
     AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.75 Safari/537.36'}
     url = requests.get('https://www.reddit.com/r/{}/about.json'.format
