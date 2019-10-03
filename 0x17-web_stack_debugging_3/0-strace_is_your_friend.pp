bad8dbb52c8901705338bf18803b3a84619b13c1
Grayson Stiegler <682@holbertonschool.com>
2019-10-03 16:43:14 +0000

    fix path for debug

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,4 @@
 #fix and debug apache
 exec { 'php':
-     command => "/bin/sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",
-     path    => "/bin",
+  command => "/bin/sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",
 }
