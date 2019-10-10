41a666da8d8dc85ce53887a324166189585ead81
Grayson Stiegler <682@holbertonschool.com>
2019-10-10 20:32:34 +0000

    fix path to command

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,4 @@
 #fix limit request limit
 exec { 'change request limit':
-  path => "/bin/sed -i 's/15/5000/g' /etc/default/nginx ; service nginx restart",
+  command => "/bin/sed -i 's/15/4096/g' /etc/default/nginx ; service nginx restart",
 }
